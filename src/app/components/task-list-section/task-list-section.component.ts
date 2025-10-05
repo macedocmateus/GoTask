34c9b4c90a6e030488d7b4edbe96cfed3f82ede3
mateus <mateusmacedoeng@gmail.com>
2025-10-05 09:13:09 -0300

    add task-card component

--- src/app/components/task-list-section/task-list-section.component.ts
+++ src/app/components/task-list-section/task-list-section.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { TaskCardComponent } from '../task-card/task-card.component'
 
 @Component({
-  selector: 'app-task-list-section',
-  imports: [],
-  templateUrl: './task-list-section.component.html',
-  styleUrl: './task-list-section.component.css'
+    selector: 'app-task-list-section',
+    imports: [TaskCardComponent],
+    templateUrl: './task-list-section.component.html',
+    styleUrl: './task-list-section.component.css',
 })
-export class TaskListSectionComponent {
-
-}
+export class TaskListSectionComponent {}
