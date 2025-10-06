24d84b5ed905ab5e149404b4c7e1e3e9ffdc4d06
mateus <mateusmacedoeng@gmail.com>
2025-10-06 20:35:58 -0300

    init task-form-modal component

--- src/app/components/task-form-modal/task-form-modal.component.ts
+++ src/app/components/task-form-modal/task-form-modal.component.ts
@@ -1,11 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-task-form-modal',
-  imports: [],
-  templateUrl: './task-form-modal.component.html',
-  styleUrl: './task-form-modal.component.css'
+    selector: 'app-task-form-modal',
+    imports: [],
+    templateUrl: './task-form-modal.component.html',
+    styleUrl: './task-form-modal.component.css',
 })
-export class TaskFormModalComponent {
-
-}
+export class TaskFormModalComponent {}
