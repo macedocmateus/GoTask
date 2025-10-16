9014f216148bd7195665bece8384152d8face3ca
mateus <mateusmacedoeng@gmail.com>
2025-10-16 17:24:16 -0300

    add task form modal interface and using open modal function

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core'
+import { ModalControllerService } from '../../services/modal-controller.service'
 
 @Component({
-  selector: 'app-task-card',
-  imports: [],
-  templateUrl: './task-card.component.html',
-  styleUrl: './task-card.component.css'
+    selector: 'app-task-card',
+    imports: [],
+    templateUrl: './task-card.component.html',
+    styleUrl: './task-card.component.css',
 })
 export class TaskCardComponent {
-
+    private readonly _modalControllerService = inject(ModalControllerService)
+    openEditTaskModal() {
+        this._modalControllerService.openEditTaskModal()
+    }
 }
--- src/app/components/welcome-section/welcome-section.component.ts
+++ src/app/components/welcome-section/welcome-section.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core'
+import { ModalControllerService } from '../../services/modal-controller.service'
 
 @Component({
-  selector: 'app-welcome-section',
-  imports: [],
-  templateUrl: './welcome-section.component.html',
-  styleUrl: './welcome-section.component.css'
+    selector: 'app-welcome-section',
+    imports: [],
+    templateUrl: './welcome-section.component.html',
+    styleUrl: './welcome-section.component.css',
 })
 export class WelcomeSectionComponent {
-
+    private readonly _modalControllerService = inject(ModalControllerService)
+    openNewTaskModal() {
+        this._modalControllerService.openNewTaskModal()
+    }
 }
